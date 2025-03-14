66e910ab8aa750d20cb38e275adb383a1ce6de1f
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-03-14 05:54:31 -0700

    multipole data post by mongodb

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) =>{
  router.post('/', async (req, res) => {
     try {
         const newTodo = new Todo(req.body);
-        await newTodo.save();  // ✅ No callback needed
+        await newTodo.save(); 
         res.status(200).json({
             message: "Todo was inserted successfully"
         });
@@ -38,12 +38,23 @@ router.get('/:id', async (req, res) =>{
 });
 
 
-
-
-
  // POST MULTIPOLE TODO 
 
-router.post('/all', async (req, res) =>{
+router.post('/all', async (req, res) =>{ 
+   
+    try {
+        const todos = await Todo.insertMany(req.body);
+        res.status(200).json({
+            message: "Todos were inserted successfully",
+            data: todos
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: "There was a server-side error",
+            details: err.message
+        });
+    }
+    
 
  })
 
