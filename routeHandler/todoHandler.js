98f355ea7057ed9292bc42f1960a84fe5e612486
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-03-14 06:22:03 -0700

    updadeed data

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -60,7 +60,27 @@ router.post('/all', async (req, res) =>{
 
  // PuT MULTIPOLE TODO 
 
-router.put('/:id', async (req, res) =>{
+router.put('/:id', async (req, res) =>{ 
+   
+    Todo.updateOne(
+        { _id: req.params.id },
+        { $set: { status: 'inactive' } }
+    )
+    .then((result) => {
+        res.status(200).json({
+            message: "Todo was updated successfully",
+            data: result
+        });
+    })
+    .catch((err) => {
+        res.status(500).json({
+            error: "There was a server-side error!",
+            details: err.message
+        });
+    });
+    
+
+     
 
  })
 
